eb6a2efc6e3436465922054d87ccab64b9ecb5fd
Blake La Pierre <blakelapierre@gmail.com>
2021-11-30 17:18:41 -0600

    new pszTimestamp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -64,7 +64,7 @@ static CBlock CreateGenesisBlock(const char *pszTimestamp,
 CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits,
                           int32_t nVersion, const Amount genesisReward) {
     const char *pszTimestamp =
-        "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
+        "hi twitter";
     const CScript genesisOutputScript =
         CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909"
                               "a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112"
